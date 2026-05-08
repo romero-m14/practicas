56f0572edefb8a1f5b5e2c084210f9508a11f1e4
MiguelR <makonisud1@gmail.com>
2026-05-08 03:29:12 -0500

    proyecto nuevos

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite' // Este es el que te daba error
+import tailwindcss from '@tailwindcss/vite'
+import {resolve} from 'path' // Este es el que te daba error
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
+  build: {
+    rollupOptions:{
+        input:{
+            master: resolve(__dirname, 'index.html'),
+            practica1: resolve(__dirname, 'practica1.html'),
+            practica2: resolve(__dirname, 'practica2.html'),
+            practica3: resolve(__dirname, 'practica3.html')
+        }
+    }
+  }
 })
